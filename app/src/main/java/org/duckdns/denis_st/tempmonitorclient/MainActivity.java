2c0f1f1569fd69abe59cc4e506e34d6e5843e7f2
Denis Stefanov <denis.stefanov@gmail.com>
2016-11-05 08:21:49 +0300

    settings_cleanup'

--- app/src/main/java/org/duckdns/denis_st/tempmonitorclient/MainActivity.java
+++ app/src/main/java/org/duckdns/denis_st/tempmonitorclient/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
